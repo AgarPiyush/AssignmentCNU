64c9115988852cc19f9fa97688572a3d775212fe
Piyush Agarwal <piyushagarwal.mnnit@gmail.com>
2016-07-20 21:15:35 +0400

    Update notifyAll.java
    Producer consumer

--- notifyAll.java
+++ notifyAll.java
@@ -10,7 +10,7 @@ package com.bugs;
  * thread in random order, it might not be the one desired. Therefore, we should instead use notifyAll() to ensure
  * all the thread wake up on that monitor, thus awekening the required thread.
  */
-
+// Template
 class notifyAll extends Thread{
 
     @Override
@@ -23,6 +23,25 @@ class notifyAll extends Thread{
                         // It might be possible that the thread that woke up is waiting for some other lock, thereby causing deadlock
         }
     }
+}
+
+public synchronized void put(Object o) {
+    while (buf.size()==MAX_SIZE) {
+        wait(); // called if the buffer is full (try/catch removed for brevity)
+    }
+    buf.add(o);
+    notify(); // called in case there are any getters or putters waiting
+}
+
+public synchronized Object get() {
+    // Y: this is where C2 tries to acquire the lock (i.e. at the beginning of the method)
+    while (buf.size()==0) {
+        wait(); // called if the buffer is empty (try/catch removed for brevity)
+        // X: this is where C1 tries to re-acquire the lock (see below)
+    }
+    Object o = buf.remove(0);
+    notify(); // called if there are any getters or putters waiting
+    return o;
 }
 
 
